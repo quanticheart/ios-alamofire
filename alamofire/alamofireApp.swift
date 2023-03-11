e831f0cf3aabc7d9aa1ca1cfd5bb56989d2feec8
Jonn Alves <jonatas.alsantos@jsl.com.br>
2023-03-11 15:49:21 -0300

    Initial Commit

---
+++ alamofire/alamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  alamofireApp.swift
+//  alamofire
+//
+//  Created by Jonn Alves on 11/03/23.
+//
+
+import SwiftUI
+
+@main
+struct alamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
